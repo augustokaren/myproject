145ff52b54b47cd9e8a2f2b0e451ddff1a39b083
Karen <karenaugusto735@gmail.com>
2024-08-23 18:52:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Formulario from "./componentes/formulario";
+
+function App() {
+  return (
+    <div className="App">
+     <Formulario/>
+ 
+
+    </div>
+
+
+
+  )
+}
+
+export default App;
---
+++ src/componentes/formulario/index.js
@@ -0,0 +1,20 @@
+function Formulario () {
+   
+    return(
+ <div>
+ 
+ 
+ <form>
+ <h1>Formulario de Cadastro</h1>
+ 
+ <label>Nome:  </label>
+ <input type="text" name= "name"  placeholder="Digite o seu nome"/>
+ 
+ </form>
+ 
+ 
+ </div>
+ )
+ }
+ 
+ export default Formulario
